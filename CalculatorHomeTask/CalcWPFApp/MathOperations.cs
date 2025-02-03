49357d0b2e72b6ac02af60a341dc88a47d531828
nedelyapyat <nedelyapyat@gmail.com>
2025-02-03 17:05:58 +0600

    дописал операцию вычитания (Subtract)

--- CalculatorHomeTask/CalcWPFApp/MathOperations.cs
+++ CalculatorHomeTask/CalcWPFApp/MathOperations.cs
@@ -18,6 +18,16 @@ namespace CalcWPFApp
             return result;
         }
 
+        public static string Substract(double a, double b)
+        {
+            string result;
+
+            var res = a - b;
+            result = res.ToString();
+
+            return result;
+        }
+
         public static string Divide(double a, double b)
         {
             if (b == 0)
--- CalculatorHomeTask/CalcWPFApp/Operations.cs
+++ CalculatorHomeTask/CalcWPFApp/Operations.cs
@@ -12,6 +12,8 @@ namespace CalcWPFApp
         Add,
         Subtract,
         Divide,
-        Multiply
+        Multiply,
+        Root,
+        Degree
     }
 }
